580ff1f8c3f51c80b8adecd4fbc66afee30cc2f9
Miha Meglic <mmeglic.miha@gmail.com>
2020-03-01 22:53:52 +0100

    Set up Config reading and writing

--- src/main/java/dev/meglic/cs/gradebook/config/Config.java
+++ src/main/java/dev/meglic/cs/gradebook/config/Config.java
@@ -1,25 +1,72 @@
 package dev.meglic.cs.gradebook.config;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public final class Config {
 	
 	private static Config instance;
 	
-	// TODO: Add config storage
+	private JSONObject config;
 	
-	// TODO: Implement file reader
+	private static String file;
+	private Logger logger;
+	
 	private Config () {
-	
+		if (System.getProperty("os.name").startsWith("Windows")) {
+			file = System.getenv("APPDATA") + "\\Meglic\\Gradebook\\config.json";
+		} else {
+			file = null;
+		}
+		
+		config = new JSONObject();
+		logger = Logger.getLogger(this.getClass().getName());
+		loadFromFile();
 	}
 	
-	// TODO: Implement getConfig()
-	public String getConfig(String parameter) {
-		return "";
+	private void loadFromFile () {
+		if (file != null) {
+			try (FileReader reader = new FileReader(file)) {
+				JSONParser parser = new JSONParser();
+				config = (JSONObject) parser.parse(reader);
+			} catch (ParseException | IOException e) {
+				logger.log(Level.WARNING, e.getMessage());
+			}
+		}
 	}
 	
-	public static Config getInstance() {
+	private void saveToFile () {
+		if (config != null && file != null) {
+			try (FileWriter writer = new FileWriter(file)) {
+				config.writeJSONString(writer);
+				writer.flush();
+			} catch (IOException e) {
+				logger.log(Level.WARNING, e.getMessage());
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String getConfig (String parameter) {
+		return (String) config.getOrDefault(parameter, null);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void setConfig (String parameter, String value) {
+		config.put(parameter, value);
+		saveToFile();
+	}
+	
+	public static Config getInstance () {
 		if (instance == null)
 			instance = new Config();
-		
 		return instance;
 	}
 }
